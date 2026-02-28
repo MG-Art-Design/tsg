11c07caa78fdf64b584c1cd0a5ad944d7a6f6a74
MG O1 <giffordmiles@gmail.com>
2026-02-28 06:18:01 +0000

    Generated by Spark: Fix all reported errors.

--- src/components/ProfileCustomization.tsx
+++ src/components/ProfileCustomization.tsx
@@ -390,14 +390,3 @@ export function ProfileCustomization({ profile, onUpdate }: ProfileCustomization
     </Card>
   )
 }
-              ))}
-            </div>
-            <Button onClick={handleCoverSave} className="w-full">
-              Save Cover Photo
-            </Button>
-          </DialogContent>
-        </Dialog>
-      </CardContent>
-    </Card>
-  )
-}
